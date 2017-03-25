afbe5191e9d4d269f7a02dbff4018b37c54f52e5
root <root@metodologia.c.stenio-157605.internal>
2017-03-25 11:56:00 +0000

    Running experiment

--- run_all_experiments.sh
+++ run_all_experiments.sh
@@ -2,7 +2,7 @@
 
 echo "algorithm,max_cpu,max_mem,max_cpu_per_bin,max_mem_per_bin,number_bins,\
 number_occupied_bins,number_of_allocated_vms,number_of_rejected_vms,\
-number_of_deleted_vms,fragmentation" > experiment_results.csv
+number_of_deleted_vms,fragmentation_cpu,fragmentation_mem" > experiment_results.csv
 
 cat experiment_inputs.txt | parallel -j+0 "python3 run_experiment.py {} >> \
                                             experiment_results.csv"
